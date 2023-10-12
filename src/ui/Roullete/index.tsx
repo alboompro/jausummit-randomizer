72b4bf3695fe1b3f3f40858db2f3c48406eb0844
João Pedro <joaopedro@MacBook-Air-de-Joao.local>
2023-10-12 19:45:31 -0300

    💄 featuring list item styles

--- src/ui/Roullete/index.tsx
+++ src/ui/Roullete/index.tsx
@@ -1,18 +1,51 @@
 import { useState } from "react";
 
-import { Container, RaffleButton } from "./styles";
+import { Container, RaffleButton, NameItem, ListContainer } from "./styles";
 
 const Roulette = () => {
-  const [myList, setMyList] = useState(names);
+  const [myList, setMyList] = useState([
+    "String 1",
+    "String 2",
+    "String 3",
+    "String 4",
+    "String 5",
+    "String 6",
+    "String 7",
+    "String 8",
+    "String 9",
+    "String 10",
+    "String 11",
+    "String 12",
+    "String 13",
+    "String 14",
+    "String 15",
+    "String 16",
+    "String 17",
+    "String 18",
+    "String 19",
+    "String 20",
+    "String 21",
+    "String 22",
+    "String 23",
+    "String 24",
+    "String 25",
+    "String 26",
+    "String 27",
+    "String 28",
+    "String 29",
+    "String 30",
+  ]);
   const [isAnimating, setIsAnimating] = useState<boolean>(false);
   const [startIndex] = useState<number>(0);
+  // const [displayedNewList, setDisplayedNewList] = useState<string[]>([]);
 
   const animateRoulette = () => {
     if (isAnimating) {
       return;
     }
 
-    const animationDuration = 1000;
+    const animationDuration = 3000;
+    // const anamationTimeout = Math.floor(animationDuration / 3);
 
     let startTime: any;
 
@@ -26,6 +59,8 @@ const Roulette = () => {
       }
 
       const elapsed = timestamp - startTime;
+      const remainingTime = animationDuration - elapsed;
+      const decelerationFactor = remainingTime / animationDuration;
 
       if (elapsed < animationDuration) {
         const progress = elapsed / animationDuration;
@@ -34,6 +69,7 @@ const Roulette = () => {
         const newList = [...shuffledList];
 
         for (let i = 0; i < position; i++) {
+          console.log(newList)
           const temp: string | undefined = newList.pop();
           if (temp) {
             newList.unshift(temp);
@@ -42,7 +78,14 @@ const Roulette = () => {
 
         setMyList(newList);
 
-        requestAnimationFrame(animate);
+        // Ajuste do progresso com base na desaceleração
+        const adjustedProgress = progress * (1 + decelerationFactor);
+
+        if (adjustedProgress <= 1) {
+          requestAnimationFrame(animate);
+        } else {
+          setIsAnimating(false);
+        }
       } else {
         setIsAnimating(false);
       }
@@ -51,23 +94,25 @@ const Roulette = () => {
     requestAnimationFrame(animate);
   };
 
-  const displayedNames = myList.slice(startIndex, startIndex + 5);
+  const displayedList = myList.slice(startIndex, startIndex + 5);
 
   return (
     <Container>
       <div>
-        <img 
-          alt="logo" 
+        <img
+          alt="logo"
           src="https://cdn.alboompro.com/630e19fcf935a50001279613_65284c94331d870001dbf5ee/original_size/jau_summit_branco-brilho-1.png?v=1"
           style={{ marginBottom: '140px' }}
         />
       </div>
 
-      <div>
-        {displayedNames.map((item, index) => (
-          <div key={index}>{item}</div>
-        ))}
-      </div>
+      <ListContainer>
+        {displayedList.map((item, index) => {
+          console.log(index)
+          return <NameItem key={index} identification={index}>{item}</NameItem>
+        }
+        )}
+      </ListContainer>
 
       <RaffleButton onClick={animateRoulette} disabled={isAnimating}>
         sortear
@@ -75,38 +120,5 @@ const Roulette = () => {
     </Container>
   );
 };
-
-const names = [
-  "String 1",
-  "String 2",
-  "String 3",
-  "String 4",
-  "String 5",
-  "String 6",
-  "String 7",
-  "String 8",
-  "String 9",
-  "String 10",
-  "String 11",
-  "String 12",
-  "String 13",
-  "String 14",
-  "String 15",
-  "String 16",
-  "String 17",
-  "String 18",
-  "String 19",
-  "String 20",
-  "String 21",
-  "String 22",
-  "String 23",
-  "String 24",
-  "String 25",
-  "String 26",
-  "String 27",
-  "String 28",
-  "String 29",
-  "String 30",
-];
 
 export default Roulette;
--- src/ui/Roullete/styles.tsx
+++ src/ui/Roullete/styles.tsx
@@ -1,4 +1,10 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+
+type NameItemProps = {
+  key: number;
+  identification: number;
+}
 
 export const Container = styled.div`
   display: flex;
@@ -32,3 +38,60 @@ export const RaffleButton = styled.button`
     background: #F91487;
   }
 `;
+
+export const ListContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+
+
+export const NameItem = styled.div<NameItemProps>`
+  ${(props) => {
+    switch (props.identification) {
+      case 0:
+        return css`
+          color: rgba(244, 241, 244, 0.60);
+          font-family: Saira;
+          font-size: 30px;
+          text-transform: uppercase;
+        `;
+      case 1:
+        return css`
+          color: rgba(244, 241, 244, 0.80);
+          font-family: Saira;
+          font-size: 50px;
+          text-transform: uppercase;
+        `;
+      case 2:
+        return css`
+          color: #00D6BA;
+          font-family: Saira;
+          font-size: 100px;
+          text-transform: uppercase;
+        `;
+      case 3:
+        return css`
+          color: rgba(244, 241, 244, 0.80);
+          font-family: Saira;
+          font-size: 50px;
+          text-transform: uppercase;
+        `;
+      case 4:
+        return css`
+          color: rgba(244, 241, 244, 0.60);
+          font-family: Saira;
+          font-size: 30px;
+          text-transform: uppercase;
+        `;
+      default:
+        return css`
+          color: rgba(244, 241, 244, 0.60);
+          font-family: Saira;
+          font-size: 30px;
+          text-transform: uppercase;
+        `;
+    }
+  }};
+`
