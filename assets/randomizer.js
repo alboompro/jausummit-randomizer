5da0f283bf658b1e986c7d067876ed858da2d2a9
João Pedro <joaopedro@MacBook-Air-de-Joao.local>
2023-10-17 18:01:18 -0300

    :sparkles: featuring new randomizer

--- assets/randomizer.js
+++ assets/randomizer.js
@@ -5,21 +5,69 @@ const allParticipants = document.querySelector("#allParticipants");
 const paragraph = document.querySelector("#paragraph");
 const imgConfetti = document.querySelector("#confetti");
 
-const storedNames = JSON.parse(localStorage.getItem("fullNames")) || [];
-const storedRegistrationNumber = JSON.parse(localStorage.getItem("registrationNumber")) || [];
+const registrationNumber = [];
+const firstName = []
+const lastName = []
+const fullName = [];
+let file = [];
+let callback;
 
-if (storedNames.length === 0) {
-  winner.textContent = "Nenhum participante cadastrado";
-  button.style.display = "none";
-  paragraph.style.display = "none";
+let isPlaying = false;
+
+const getAllNames = async () => {
+  const res = await fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vS6c0coxJlTiD0iSMrFtjdc1AxEOOV8vFbvkjJUn4ao2Ktg4eUxQcGp422pm-tgclp3z837jlOgbL4j/pub?gid=0&single=true&output=csv", {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'text/csv;charset=UTF-8'
+    }
+  });
+  const data = await res.text();
+  if (data) {
+    const lines = data.split("\n");
+    const names = [];
+
+    for (const line of lines) {
+      const parts = line.split(";");
+      for (const part of parts) {
+        const trimmedPart = part.trim();
+        if (trimmedPart !== "") {
+          names.push(trimmedPart);
+        }
+      }
+    }
+
+    const sliceNames = names.slice(1);
+
+    sliceNames.forEach((partcipants, index) => {
+
+      const splitComma = partcipants.split(',')
+
+      registrationNumber.push(splitComma[0])
+      firstName.push(splitComma[1])
+      lastName.push(splitComma[2])
+    });
+
+    if (firstName.length === lastName.length) {
+      for (let i = 0; i < firstName.length; i++) {
+        const trimmedFirstName = firstName[i].trim();
+        const trimmedLastName = lastName[i].trim();
+        const fullNameStr = `${trimmedFirstName} ${trimmedLastName}`;
+        fullName.push(fullNameStr);
+      }
+    } else {
+      winner.textContent = "Nenhum participante cadastrado";
+      button.style.display = "none";
+      paragraph.style.display = "none";
+    }
+
+    allParticipants.textContent = fullName.length;
+    allParticipants.style.margin = "0 4px";
+  }
 }
 
-allParticipants.textContent = storedNames.length;
-allParticipants.style.margin = "0 4px";
-
 const randomName = () => {
-  const rand = Math.floor(Math.random() * storedNames.length);
-  const name = storedNames[rand];
+  const rand = Math.floor(Math.random() * fullName.length);
+  const name = fullName[rand];
   if (name.length > 30) {
     winner.style.display = "flex";
     winner.style.flexDirection = "column";
@@ -27,9 +75,8 @@ const randomName = () => {
   }
 
   winner.style.textTransform = "capitalize"
-
   winner.textContent = name;
-  registration.textContent = storedRegistrationNumber[rand];
+  registration.textContent = registrationNumber[rand];
 };
 
 const setDeceleratingTimeout = (callback, times) => {
@@ -50,15 +97,13 @@ const timeOutCallBack = () => {
     winner.textContent = winner.innerText;
     imgConfetti.style.display = "block";
     imgConfetti.setAttribute("src", "assets/images/Celebration.gif");
-    setTimeout(function () {
+    callback = setTimeout(function () {
       isPlaying = false;
       imgConfetti.setAttribute("src", "");
       imgConfetti.style.display = "none";
     }, 5000);
   };
 };
-
-let isPlaying = false;
 
 button.addEventListener("click", (e) => {
   if (isPlaying) return;
@@ -74,3 +119,17 @@ button.addEventListener("click", (e) => {
 
   e.preventDefault();
 });
+
+document.addEventListener("keydown", function (event) {
+  if (event.code === "PageDown" || event.code === "PageUp") {
+    button.click();
+    if (isPlaying === true) {
+      isPlaying = false;
+      imgConfetti.setAttribute("src", "");
+      imgConfetti.style.display = "none";
+      window.clearTimeout(callback);
+    }
+  }
+});
+
+getAllNames();
--- assets/upload.js
+++ assets/upload.js
@@ -2,62 +2,71 @@ const registrationNumber = []
 const firstName = []
 const lastName = []
 
-document.getElementById("fileInput").addEventListener("change", function () {
-  const fileInput = document.getElementById("fileInput");
-  const file = fileInput.files[0];
 
-  if (file) {
-    if (file.name.endsWith(".csv")) {
-      const reader = new FileReader();
-
-      reader.onload = function (e) {
-        const csvContent = e.target.result;
-        const lines = csvContent.split("\n");
-        const names = [];
-
-        for (const line of lines) {
-          const parts = line.split(";");
-          for (const part of parts) {
-            const trimmedPart = part.trim();
-            if (trimmedPart !== "") {
-              names.push(trimmedPart);
-            }
-          }
-        }
-
-        const sliceNames = names.slice(1);
-        
-        sliceNames.forEach((partcipants, index) => {
-
-          const splitComma = partcipants.split(',')
-
-          registrationNumber.push(splitComma[0])
-          firstName.push(splitComma[1])
-          lastName.push(splitComma[2])
-        });
-
-        if (firstName.length === lastName.length) {
-          const fullName = [];
-        
-          for (let i = 0; i < firstName.length; i++) {
-            const trimmedFirstName = firstName[i].trim();
-            const trimmedLastName = lastName[i].trim();
-            const fullNameStr = `${trimmedFirstName} ${trimmedLastName}`;
-            fullName.push(fullNameStr);
-          }
-
-          localStorage.setItem('registrationNumber', JSON.stringify(registrationNumber));
-          localStorage.setItem('fullNames', JSON.stringify(fullName));
-          debugger
-          window.location.href = 'index.html';
-        } else {
-          console.error("Os arrays firstName e lastName não têm o mesmo comprimento.");
-        }
-      };
-
-      reader.readAsText(file);
-    } else {
-      alert("Por favor, selecione um arquivo CSV.");
+const getAllNames = async () => {
+  const res = await fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vS6c0coxJlTiD0iSMrFtjdc1AxEOOV8vFbvkjJUn4ao2Ktg4eUxQcGp422pm-tgclp3z837jlOgbL4j/pub?gid=0&single=true&output=csv", {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'text/csv;charset=UTF-8'
     }
+  });
+  return await res.json();
+}
+
+const file = getAllNames();
+
+console.log(file)
+
+if (file) {
+  if (file.name.endsWith(".csv")) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const csvContent = e.target.result;
+      const lines = csvContent.split("\n");
+      const names = [];
+
+      for (const line of lines) {
+        const parts = line.split(";");
+        for (const part of parts) {
+          const trimmedPart = part.trim();
+          if (trimmedPart !== "") {
+            names.push(trimmedPart);
+          }
+        }
+      }
+
+      const sliceNames = names.slice(1);
+      
+      sliceNames.forEach((partcipants, index) => {
+
+        const splitComma = partcipants.split(',')
+
+        registrationNumber.push(splitComma[0])
+        firstName.push(splitComma[1])
+        lastName.push(splitComma[2])
+      });
+
+      if (firstName.length === lastName.length) {
+        const fullName = [];
+      
+        for (let i = 0; i < firstName.length; i++) {
+          const trimmedFirstName = firstName[i].trim();
+          const trimmedLastName = lastName[i].trim();
+          const fullNameStr = `${trimmedFirstName} ${trimmedLastName}`;
+          fullName.push(fullNameStr);
+        }
+
+        localStorage.setItem('registrationNumber', JSON.stringify(registrationNumber));
+        localStorage.setItem('fullNames', JSON.stringify(fullName));
+        window.location.href = 'index.html';
+      } else {
+        console.error("Os arrays firstName e lastName não têm o mesmo comprimento.");
+      }
+    };
+
+    reader.readAsText(file);
+  } else {
+    alert("Por favor, selecione um arquivo CSV.");
   }
-});
+}
